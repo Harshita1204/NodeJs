9f1ffc472ebd0eb0c157f3fb6167fd6ae3d601c8
Harshita1204 <harshitak1204@gmail.com>
2026-02-06 22:59:15 +0530

    nodejscode1.js

--- nodejscode1.js
+++ nodejscode1.js
@@ -1,2 +1,6 @@
 console.log("Node.js is running successfully!");
 console.log("Current Date:", new Date().toLocaleDateString());
+
+// output 
+// Node.js is running successfully!
+// Current Date: 6/2/2026
